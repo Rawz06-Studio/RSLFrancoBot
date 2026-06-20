ea97f482d6e826c984c780fa7678754de899103d
RawZ06 <longordo.alexandre@gmail.com>
2026-06-20 23:10:27 +0200

    Add ao and zorro to rot

--- src/main/java/fr/rawz06/rslfrancobot/engine/usecases/visibility/GetUserAvailableGenerateUseCase.java
+++ src/main/java/fr/rawz06/rslfrancobot/engine/usecases/visibility/GetUserAvailableGenerateUseCase.java
@@ -8,7 +8,7 @@ import java.util.Map;
 @Component
 public class GetUserAvailableGenerateUseCase {
 
-    Map<String, List<String>> availability = Map.of("rot", List.of("rawz06", "manu_4486", "greenpepperch", "blueguy0014", "tonio9193"));
+    Map<String, List<String>> availability = Map.of("rot", List.of("rawz06", "manu_4486", "greenpepperch", "blueguy0014", "tonio9193", "aosuna", "zorrodisco"));
 
     public boolean available(String user, String preset) {
         return availability.get(preset).contains(user);
